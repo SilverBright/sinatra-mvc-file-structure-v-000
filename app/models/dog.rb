700c8948a35b3b3d68c88648e09667f09526f990
Barbara Washington <barbara.m.washington@gmail.com>
2018-07-18 23:18:04 +0000

    automatically backed up by learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -2,6 +2,16 @@ class Dog
 
     attr_accessor :name, :breed, :age
 
+    def initialize
+      @name = name
+      @breed = breed
+      @age = age
+    end
+
+    # def self.all
+    #   @@all
+    # end
+
 
 
 end
